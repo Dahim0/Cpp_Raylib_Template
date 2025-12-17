28ca488431688babfa31527aecb235e14d94b0f1
Dahim0 <147000459+Dahim0@users.noreply.github.com>
2025-12-17 18:57:25 -0500

    Clearing

--- README.md
+++
@@ -1,2 +0,0 @@
-# Cpp_Raylib_Template
-This is a clean and easy Cpp raylib template for anyone who wanna get started fast
--- main.cpp
+++ main.cpp
@@ -1,26 +1,19 @@
 #include "raylib.h"
-#include <cmath>
-#include <string> //what type of language need to include string -_-
-#include <iostream>
-#include <vector>
-using namespace std;
-
 
 int main() {
 
-    InitWindow(500, 500, "Game");
-    SetTargetFPS(60);
-    
-    while (!WindowShouldClose()) {
+    InitWindow(500, 500, "Game"); //Setting up the game windows size and name
+    SetTargetFPS(60); //Max fps
+
+    while (!WindowShouldClose()) { //Main game loop
         BeginDrawing();
 
         ClearBackground(WHITE);
         DrawText("Hello World!",10,10,50,BLACK);
 
         EndDrawing();
-
     }
 
     CloseWindow();
-    return 0;//as much this is useless it use important
+    return 0;
 }
